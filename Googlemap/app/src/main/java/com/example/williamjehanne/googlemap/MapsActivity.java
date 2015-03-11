554ed433c936fab9fd611bd8078e25d622d4dd91
williamjehanne <william.jehanne@gmail.com>
2015-03-11 17:19:52 +0100

    add google map repository

---
+++ Googlemap/app/src/main/java/com/example/williamjehanne/googlemap/MapsActivity.java
@@ -0,0 +1,90 @@
+package com.example.williamjehanne.googlemap;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        setUpMapIfNeeded();
+
+        Polyline line = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(48.862430200000000000, 2.287181700000019000), new LatLng(48.869916000000000000, 2.285929000000010000))
+             /*   .add(new LatLng(51.5, -0.1), new LatLng(40.7, -74.0))*/
+                .width(20)
+                .color(Color.parseColor("#742189")));
+
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(48.862430200000000000, 2.287181700000019000))
+                .radius(30)
+                .strokeColor(Color.parseColor("#742189"))
+                .fillColor(Color.parseColor("#742189")));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        LatLng latlng = new LatLng(48.858370099999990000, 2.294481300000029500);
+        mMap.addMarker(new MarkerOptions().position(latlng).title("Tour eiffel")).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, 15));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14), 2000, null);
+    }
+}
